f1fbecb2c096bcb906a5c0021224ac03459788a8
vishalgit <vishal.reply@outlook.com>
2023-12-29 21:48:57 +0530

    configured undofile

--- plugin/kickstart-adjunct.lua
+++ plugin/kickstart-adjunct.lua
@@ -4,6 +4,7 @@ vim.o.tabstop = 2
 vim.o.softtabstop = 2
 vim.o.shiftwidth = 2
 vim.o.expandtab = true
+vim.o.undofile = false
 
 -- Configure keymaps
 vim.keymap.set({ 'i', 'v' }, 'jk', '<Esc>', { silent = true, desc = 'Mapping jk to Esc key' })
