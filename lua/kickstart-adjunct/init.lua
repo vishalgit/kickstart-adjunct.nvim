1a987f57312019df64c8c8b1358f8dac03c8f350
vishalgit <vishal.reply@outlook.com>
2023-12-29 21:27:36 +0530

    Initial Commit

---
+++ lua/kickstart-adjunct/init.lua
@@ -0,0 +1,7 @@
+local M = {}
+
+function M.setup()
+	print('Kickstart Adjunct Setup Called')
+end
+
+return M
---
+++ plugin/kickstart-adjunct.lua
@@ -0,0 +1,6 @@
+-- Configure options to add and override
+
+vim.o.tabstop = 2
+vim.o.softtabstop = 2
+vim.o.shiftwidth = 2
+vim.o.expandtab = true
